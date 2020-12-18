52f453e04f871c3f5c5110dab817e6c83bb89828
KotenkoNata <kotenkonata8@gmail.com>
2020-12-18 22:32:41 +0200

    Create the timer

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import "./styles.css";
+
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
+
+const CountdownTimer = function ({ selector, targetDate }) {
+  const date = targetDate.getTime();
+  const refs = {
+    days: document.querySelector(`${selector} [data-value=days]`),
+    hours: document.querySelector(`${selector} [data-value=hours]`),
+    minutes: document.querySelector(`${selector} [data-value=mins]`),
+    seconds: document.querySelector(`${selector} [data-value=secs]`),
+    updateTime(time) {
+      const millisInSec = 1000;
+      const millisInMinute = millisInSec * 60;
+      const millisInHour = millisInMinute * 60;
+      const millisInDay = millisInHour * 24;
+
+      const days = pad(Math.floor(time / millisInDay));
+      const hours = pad(Math.floor((time % millisInDay) / millisInHour));
+      const mins = pad(Math.floor((time % millisInHour) / millisInMinute));
+      const secs = pad(Math.floor((time % millisInMinute) / millisInSec));
+
+      this.days.textContent = days;
+      this.hours.textContent = hours;
+      this.minutes.textContent = mins;
+      this.seconds.textContent = secs;
+    },
+  };
+
+  refs.updateTime(0);
+
+  const intervalId = setInterval(() => {
+    const currentDate = Date.now();
+    const time = date - currentDate;
+    if (time < 0) {
+      clearInterval(intervalId);
+      console.log("interval closed");
+      return;
+    }
+    refs.updateTime(time);
+  }, 1000);
+};
+
+new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date(2020, 11, 31, 23, 59, 59, 0),
+});
